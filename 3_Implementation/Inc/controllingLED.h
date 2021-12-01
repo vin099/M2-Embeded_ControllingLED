3597c76d9586b4d03c3a2698652e9d6be4105407
vin099 <94450629+vin099@users.noreply.github.com>
2021-12-01 13:01:39 +0530

    Update controllingLED.h

--- 3_Implementation/Inc/controllingLED.h
+++ 3_Implementation/Inc/controllingLED.h
@@ -1,1 +1,11 @@
+#ifndef _controllingLED_H_
+#define _controllingLED_H_
 
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+void controllingLED_on();
+void controllingLED_off();
+
+#endif
